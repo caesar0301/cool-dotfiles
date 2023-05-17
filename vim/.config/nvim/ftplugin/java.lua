567e029574d8620c4d31d12ff2d80c6982007fb1
chenxm <chenxm35@gmail.com>
2023-05-17 16:31:15 +0800

    Optimize nvim-jdtls config

--- vim/.config/nvim/ftplugin/java.lua
+++ vim/.config/nvim/ftplugin/java.lua
@@ -1,6 +1,60 @@
+local jdtls = require('jdtls')
+local root_markers = {'gradlew', '.git', 'gradlew'}
 local home = os.getenv('HOME')
+local jdtls_install_folder = home .. "/.local/share/jdt-language-server"
+local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 local config = {
-    cmd = {home .. "/.local/share/jdt-language-server/bin/jdtls"},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
+    -- Java17+
+    cmd = {
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product', 
+        '-Dlog.protocol=true', 
+        '-Dlog.level=ALL', 
+        '-Xmx4g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens',
+        'java.base/java.util=ALL-UNNAMED',
+        '--add-opens',
+        'java.base/java.lang=ALL-UNNAMED',
+        '-jar', vim.fn.glob(jdtls_install_folder .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+        '-configuration', jdtls_install_folder .. '/config_linux', '-data', workspace_folder
+    },
+
+    root_dir = jdtls.setup.find_root(root_markers),
+
+    -- Here you can configure eclipse.jdt.ls specific settings
+    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+    -- for a list of options
+    settings = {
+        java = {}
+    },
+
+    -- Language server `initializationOptions`
+    -- You need to extend the `bundles` with paths to jar files
+    -- if you want to use additional eclipse.jdt.ls plugins.
+    -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+    init_options = {
+        bundles = {}
+    },
+
+    on_attach = function(client, bufnr)
+        jdtls.setup_dap({
+            hotcodereplace = 'auto'
+        })
+        jdtls.setup.add_commands()
+        local opts = {
+            silent = true,
+            buffer = bufnr
+        }
+        vim.keymap.set('n', "cdo", jdtls.organize_imports, opts)
+        vim.keymap.set('n', "cdf", jdtls.test_class, opts)
+        vim.keymap.set('n', "cdn", jdtls.test_nearest_method, opts)
+        vim.keymap.set('n', "crv", jdtls.extract_variable, opts)
+        vim.keymap.set('v', 'crm', [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], opts)
+        vim.keymap.set('n', "crc", jdtls.extract_constant, opts)
+    end
 }
-require('jdtls').start_or_attach(config)
+-- This starts a new client & server,
+jdtls.start_or_attach(config)
