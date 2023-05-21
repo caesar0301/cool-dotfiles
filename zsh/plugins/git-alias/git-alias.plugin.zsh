41a19f41c4d937dbb22d2e18408dad70ff706818
xiaming.chen <chenxm35@gmail.com>
2023-05-21 16:36:23 +0800

    Add more git alias

--- zsh/plugins/git-alias/git-alias.plugin.zsh
+++ zsh/plugins/git-alias/git-alias.plugin.zsh
@@ -12,8 +12,21 @@ alias ghf="git log --follow -p --"
 alias grsh="git reset --soft HEAD^ && git reset --hard HEAD"
 alias gsrh="git submodule foreach --recursive git reset --hard"
 alias gsur="git submodule update --init --recursive"
-alias git-quick-update="git add -u && git commit -m \"Quick update\" && git push"
+
 alias gqu="git-quick-update"
+alias git_quick_update="git add -u && git commit -m \"Quick update\" && git push"
+alias git_submodule_latest="git submodule foreach git pull origin master"
+
+function git_reset_recurse_submodules {
+    #Cleans and resets a git repo and its submodules
+    #https://gist.github.com/nicktoumpelis/11214362
+    git reset --hard
+    git submodule sync --recursive
+    git submodule update --init --force --recursive
+    git clean -ffdx
+    git submodule foreach --recursive git clean -ffdx
+}
+
 
 # Remove deleted file from git cache
 function git_prune_cache {
