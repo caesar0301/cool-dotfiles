91d2a9de7cdb2c87193c7ec1dc89ce041039cd97
chenxm <chenxm35@gmail.com>
2023-06-25 15:09:04 +0800

    Fix nvim-tree quits as last buffer

--- neovim/.config/nvim/plugin/nvim-tree.lua
+++ neovim/.config/nvim/plugin/nvim-tree.lua
@@ -5,6 +5,30 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+-- Quit as last buffer, fix issue by @ppwwyyxx
+-- https://github.com/nvim-tree/nvim-tree.lua/issues/1368#issuecomment-1512248492
+vim.api.nvim_create_autocmd(
+    "QuitPre",
+    {
+        callback = function()
+            local invalid_win = {}
+            local wins = vim.api.nvim_list_wins()
+            for _, w in ipairs(wins) do
+                local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+                if bufname:match("NvimTree_") ~= nil then
+                    table.insert(invalid_win, w)
+                end
+            end
+            if #invalid_win == #wins - 1 then
+                -- Should quit, so we close all invalid windows.
+                for _, w in ipairs(invalid_win) do
+                    vim.api.nvim_win_close(w, true)
+                end
+            end
+        end
+    }
+)
+
 local function my_on_attach(bufnr)
     local api = require "nvim-tree.api"
     local function opts(desc)
