2c9a2e7f288ba27a28dd438c3cca425358d366b4
chenxm <chenxm35@gmail.com>
2023-07-17 15:05:00 +0800

    Add replace cmd

--- zsh/.config/zsh_runtime/plugins/common-helper/common-helper.plugin.zsh
+++ zsh/.config/zsh_runtime/plugins/common-helper/common-helper.plugin.zsh
@@ -8,3 +8,10 @@ alias sd="sdcv -0 -c"
 
 # Rsync preseving symlinks, timestamps, permissions
 alias rsync2="rsync -rlptgoD --progress"
+
+function replace {
+    sourcestr="$1"
+    targetstr="$2"
+    pathstr="$3"
+    grep -r $sourcestr $pathstr | awk -F: '{print $1}' | uniq | xargs -i sed -i -E "s|$sourcestr|$targetstr|g" {}
+}
