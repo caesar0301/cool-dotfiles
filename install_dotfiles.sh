b083e31617b349fde1b9aa232a66d4cefcecdf98
xiaming.chen <chenxm35@gmail.com>
2023-09-09 14:48:45 +0800

    Quick update

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -41,6 +41,7 @@ function install_pyenv() {
     if [ ! -e $HOME/.pyenv ]; then
         info "Installing pyenv to $HOME/.pyenv..."
         git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+        git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pyenv-virtualenv
     fi
 }
 
