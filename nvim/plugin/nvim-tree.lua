416ab1de52cbe387021c9f6813692fa697a24d0c
xiaming.chen <chenxm35@gmail.com>
2023-09-23 20:46:45 +0800

    Quick update

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -48,6 +48,11 @@ require("nvim-tree").setup {
     renderer = {
         group_empty = true
     },
+    update_focused_file = {
+        enable = true,
+        update_root = false,
+        ignore_list = {}
+    },
     filters = {
         dotfiles = false
     }
