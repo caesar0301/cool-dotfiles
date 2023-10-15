1c14654b4c0918aa77ce85b20c3470e320009afa
xiaming.chen <chenxm35@gmail.com>
2023-10-15 17:22:50 +0800

    Quick update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,24 +6,91 @@ return require("packer").startup(
         use "wbthomason/packer.nvim"
 
         ---------------------------
-        -- Themes
+        -- Editing
         ---------------------------
-        use "dracula/vim"
-        use "altercation/vim-colors-solarized"
-        use "morhetz/gruvbox"
-        use "therubymug/vim-pyte"
-        use "vim-scripts/mayansmoke"
-        use "tomasiser/vim-code-dark"
-        use "junegunn/rainbow_parentheses.vim"
-        use "itchyny/lightline.vim"
-        use "nvim-lua/lsp-status.nvim"
-        use "jlanzarotta/bufexplorer"
+        use "maxbrunsfeld/vim-yankstack"
+        use "tpope/vim-commentary"
+        use "tpope/vim-repeat"
+        use "tpope/vim-surround"
+
+        ---------------------------
+        -- Navigation
+        ---------------------------
         use {
-            "romgrk/barbar.nvim",
+            "nvim-tree/nvim-tree.lua",
             requires = {
-                {"nvim-tree/nvim-web-devicons"}
+                {"nvim-tree/nvim-web-devicons"},
+                {"antosha417/nvim-lsp-file-operations", opt = true},
+                {"echasnovski/mini.base16", opt = true}
             }
         }
+        use "preservim/tagbar"
+        use {
+            -- Complement tagbar
+            "simrat39/symbols-outline.nvim"
+        }
+        use {
+            "junegunn/fzf.vim",
+            requires = {"junegunn/fzf", run = ":call fzf#install()"},
+            rtp = "~/.fzf"
+        }
+        use {
+            "nvim-telescope/telescope.nvim",
+            tag = "0.1.1",
+            requires = {{"nvim-lua/plenary.nvim"}}
+        }
+        use "amix/vim-zenroom2"
+        use "junegunn/goyo.vim"
+        use "pechorin/any-jump.vim"
+        use "terryma/vim-expand-region" -- Usage: +/- to expand/shrink
+        use {"mg979/vim-visual-multi", branch = "master"}
+        use "michaeljsmith/vim-indent-object"
+        use "preservim/vim-indent-guides"
+        use "vim-scripts/mru.vim"
+        use {
+            "m-demare/hlargs.nvim",
+            config = function()
+                require("hlargs").setup()
+            end
+        }
+
+        ---------------------------
+        -- Languages
+        ---------------------------
+        use "vim-autoformat/vim-autoformat"
+        use "Olical/conjure"
+        use "bhurlow/vim-parinfer"
+        use "Vimjas/vim-python-pep8-indent"
+        use "cdelledonne/vim-cmake"
+        use "chr4/nginx.vim"
+        use "chrisbra/csv.vim"
+        use "digitaltoad/vim-pug"
+        use "editorconfig/editorconfig-vim"
+        use "groenewege/vim-less"
+        use "jalvesaq/Nvim-R"
+        use "kchmck/vim-coffee-script"
+        use "leafgarland/typescript-vim"
+        use "lervag/vimtex"
+        use "mfussenegger/nvim-jdtls"
+        use "pangloss/vim-javascript"
+        use "plasticboy/vim-markdown"
+        use "rust-lang/rust.vim"
+        use "neovimhaskell/haskell-vim"
+        use "nvie/vim-flake8"
+        use "vim-ruby/vim-ruby"
+        use "tarekbecker/vim-yaml-formatter"
+        use "sophacles/vim-bundle-mako"
+        use {"vlime/vlime", rtp = "vim/"}
+
+        -- preview markdown
+        use(
+            {
+                "iamcco/markdown-preview.nvim",
+                run = function()
+                    vim.fn["mkdp#util#install"]()
+                end
+            }
+        )
 
         ---------------------------
         -- LSP and improvement
@@ -78,91 +145,24 @@ return require("packer").startup(
         }
 
         ---------------------------
-        -- Navigation
+        -- Themes
         ---------------------------
+        use "dracula/vim"
+        use "altercation/vim-colors-solarized"
+        use "morhetz/gruvbox"
+        use "therubymug/vim-pyte"
+        use "vim-scripts/mayansmoke"
+        use "tomasiser/vim-code-dark"
+        use "junegunn/rainbow_parentheses.vim"
+        use "itchyny/lightline.vim"
+        use "nvim-lua/lsp-status.nvim"
+        use "jlanzarotta/bufexplorer"
         use {
-            "nvim-tree/nvim-tree.lua",
+            "romgrk/barbar.nvim",
             requires = {
-                {"nvim-tree/nvim-web-devicons"},
-                {"antosha417/nvim-lsp-file-operations", opt = true},
-                {"echasnovski/mini.base16", opt = true}
+                {"nvim-tree/nvim-web-devicons"}
             }
         }
-        use "preservim/tagbar"
-        use {
-            -- Complement tagbar
-            "simrat39/symbols-outline.nvim"
-        }
-        use {
-            "junegunn/fzf.vim",
-            requires = {"junegunn/fzf", run = ":call fzf#install()"},
-            rtp = "~/.fzf"
-        }
-        use {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.1",
-            requires = {{"nvim-lua/plenary.nvim"}}
-        }
-        use "amix/vim-zenroom2"
-        use "junegunn/goyo.vim"
-        use "pechorin/any-jump.vim"
-        use "terryma/vim-expand-region" -- Usage: +/- to expand/shrink
-        use {"mg979/vim-visual-multi", branch = "master"}
-        use "michaeljsmith/vim-indent-object"
-        use "preservim/vim-indent-guides"
-        use "vim-scripts/mru.vim"
-        use {
-            "m-demare/hlargs.nvim",
-            config = function()
-                require("hlargs").setup()
-            end
-        }
-
-        ---------------------------
-        -- Editing
-        ---------------------------
-        use "maxbrunsfeld/vim-yankstack"
-        use "tpope/vim-commentary"
-        use "tpope/vim-repeat"
-        use "tpope/vim-surround"
-
-        ---------------------------
-        -- Languages
-        ---------------------------
-        use "vim-autoformat/vim-autoformat"
-        use "Olical/conjure"
-        use "bhurlow/vim-parinfer"
-        use "Vimjas/vim-python-pep8-indent"
-        use "cdelledonne/vim-cmake"
-        use "chr4/nginx.vim"
-        use "chrisbra/csv.vim"
-        use "digitaltoad/vim-pug"
-        use "editorconfig/editorconfig-vim"
-        use "groenewege/vim-less"
-        use "jalvesaq/Nvim-R"
-        use "kchmck/vim-coffee-script"
-        use "leafgarland/typescript-vim"
-        use "lervag/vimtex"
-        use "mfussenegger/nvim-jdtls"
-        use "pangloss/vim-javascript"
-        use "plasticboy/vim-markdown"
-        use "rust-lang/rust.vim"
-        use "neovimhaskell/haskell-vim"
-        use "nvie/vim-flake8"
-        use "vim-ruby/vim-ruby"
-        use "tarekbecker/vim-yaml-formatter"
-        use "sophacles/vim-bundle-mako"
-        use {"vlime/vlime", rtp = "vim/"}
-
-        -- preview markdown
-        use(
-            {
-                "iamcco/markdown-preview.nvim",
-                run = function()
-                    vim.fn["mkdp#util#install"]()
-                end
-            }
-        )
 
         ---------------------------
         -- Misc
