b5843533808220db40814678fdbeb223ef88e182
道夕 <xiaming.cxm@alibaba-inc.com>
2023-10-16 18:12:44 +0800

    Quick update

--- nvim/plugin/nvim-cmp.lua
+++ nvim/plugin/nvim-cmp.lua
@@ -7,7 +7,10 @@ cmp.setup(
                 require("luasnip").lsp_expand(args.body)
             end
         },
-        window = {},
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered()
+        },
         mapping = cmp.mapping.preset.insert(
             {
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
