63684964d588d55f585eae12e21d6dc9488391eb
道夕 <xiaming.cxm@alibaba-inc.com>
2023-11-21 11:31:50 +0800

    Quick update

--- zsh/bundles/dev-tools.zsh
+++ zsh/bundles/dev-tools.zsh
@@ -77,10 +77,10 @@ _initHaskellEnv
 
 #if [[ $OSTYPE == darwin*  ]]; then _initMacEnv; fi
 
-run_http_server() {
+local-http-server() {
 	# Check python major version
 	PYV=$(python -c "import sys;t='{v[0]}'.format(v=list(sys.version_info[:1]));sys.stdout.write(t)")
-	if [ "$PYV" == "3" ]; then
+	if [ "$PYV" -eq "3" ]; then
 		python -m http.server 8899
 	else
 		python -m SimpleHTTPServer 8899
