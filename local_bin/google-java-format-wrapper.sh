bfd630101eb11b82f27b58b43864bab1a35e46a4
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2023-12-28 10:42:09 +0800

    add google-java-format-wrapper

---
+++ local_bin/google-java-format-wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+JDK_HOME=${JAVA_HOME_4GJF}
+if [ x$JDK_HOME == "x" ]; then
+    JDK_HOME=${JAVA_HOME}
+fi
+
+JAVABIN="java"
+if [ x$JDK_HOME != "x" ]; then
+    JAVABIN=$JDK_HOME/bin/java
+fi
+
+GJFJAR="${GJF_JAR_FILE}"
+if [ x$GJFJAR == "x" ]; then
+    GJFJAR=$HOME/.local/share/google-java-format/google-java-format-all-deps.jar
+fi
+
+$JAVABIN -jar $GJFJAR - $@
