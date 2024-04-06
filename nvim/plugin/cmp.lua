4ff16e36fde48eb48508f8bdea41001a5fbcedcb
xiaming.chen <chenxm35@gmail.com>
2024-04-06 17:50:03 +0800

    Quick update

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -13,19 +13,14 @@ cmp.setup(
         },
         mapping = cmp.mapping.preset.insert(
             {
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-y>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({select = true}),
                 ["<Tab>"] = cmp.mapping(
                     function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
                         elseif luasnip.expand_or_locally_jumpable() then
                             luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
                         else
                             fallback()
                         end
@@ -43,15 +38,27 @@ cmp.setup(
                         end
                     end,
                     {"i", "s"}
-                )
+                ),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+                ["<C-y>"] = cmp.config.disable,
+                ["<C-e>"] = cmp.mapping(
+                    {
+                        i = cmp.mapping.abort(),
+                        c = cmp.mapping.close()
+                    }
+                ),
+                ["<CR>"] = cmp.mapping.confirm({select = true})
             }
         ),
         sources = cmp.config.sources(
             {
                 {name = "nvim_lsp"},
-                {name = "luasnip"}
-            },
-            {
+                {name = "luasnip"},
+                {name = "path"},
                 {name = "buffer"}
             }
         )
