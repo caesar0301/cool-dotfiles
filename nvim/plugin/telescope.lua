b58a149456671209f66a0a6fe49168e1c90d2214
道夕 <xiaming.cxm@alibaba-inc.com>
2024-04-09 18:07:56 +0800

    quit telescope with single <esc>

---
+++ nvim/plugin/telescope.lua
@@ -0,0 +1,13 @@
+local actions = require("telescope.actions")
+
+require("telescope").setup(
+    {
+        defaults = {
+            mappings = {
+                i = {
+                    ["<esc>"] = actions.close
+                }
+            }
+        }
+    }
+)
