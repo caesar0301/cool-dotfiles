53efbcea4be9d9c90d7469a645ad45a9a69fff56
道夕 <xiaming.cxm@alibaba-inc.com>
2024-04-15 17:36:38 +0800

    Quick update

--- nvim/lua/caesardev/keymaps.lua
+++ nvim/lua/caesardev/keymaps.lua
@@ -133,7 +133,7 @@ keymap.set(
 
 -- Use Telescope to search the provided path
 api.nvim_create_user_command(
-    "GrepDir",
+    "FindDir",
     function(opts)
         local builtin = require("telescope.builtin")
         builtin.live_grep({prompt_title = "Search in " .. opts.fargs[1], cwd = opts.fargs[1]})
