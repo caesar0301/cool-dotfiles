f2b1d8020e7a5a39acd3439bc143583cf3c731e9
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2024-08-19 13:41:09 +0800

    update neovide config

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -2,3 +2,9 @@
 vim.print(vim.g.neovide_version)
 --vim.o.guifont = "Source Code Pro:h14"
 vim.g.neovide_remember_window_size = true
+
+-- animation
+vim.g.neovide_scroll_animation_length = 0.1
+vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_cursor_trail_size = 0.5
+vim.g.neovide_cursor_animate_command_line = false
