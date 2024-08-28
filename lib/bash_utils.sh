21e1f0021248d6e35a5fdd21818dbc93f6f6818f
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2024-08-28 11:28:01 +0800

    Add tmux installer

--- lib/bash_utils.sh
+++ lib/bash_utils.sh
@@ -2,134 +2,141 @@
 
 # colored messages
 function warn {
-    YELLOW='\033[0;33m'
-    NC='\033[0m' # No Color
-    printf "${YELLOW}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
+  YELLOW='\033[0;33m'
+  NC='\033[0m' # No Color
+  printf "${YELLOW}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
 }
 
 function info {
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
-    printf "${GREEN}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
+  GREEN='\033[0;32m'
+  NC='\033[0m' # No Color
+  printf "${GREEN}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
 }
 
 function error {
-    RED='\033[0;31m'
-    NC='\033[0m' # No Color
-    printf "${RED}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  printf "${RED}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
 }
 
 # Open file window
 function openw {
-    KNAME=$(uname -s)
-    KREL=$(uname -r)
-    EXE='nautilus'
-    if [[ $KNAME == "Linux" ]]; then
-        if [[ $KREL =~ "microsoft-standard" ]]; then
-            EXE='explorer.exe'
-        fi
-    elif [[ $KNAME == "Darwin" ]]; then
-        EXE='open'
+  KNAME=$(uname -s)
+  KREL=$(uname -r)
+  EXE='nautilus'
+  if [[ $KNAME == "Linux" ]]; then
+    if [[ $KREL =~ "microsoft-standard" ]]; then
+      EXE='explorer.exe'
     fi
-    $EXE $@
+  elif [[ $KNAME == "Darwin" ]]; then
+    EXE='open'
+  fi
+  $EXE $@
 }
 
 # Grep and replace
 function greprp {
-    if [ $# -eq 2 ]; then
-        spath='.'
-        oldstr=$1
-        newstr=$2
-    elif [ $# -eq 3 ]; then
-        spath=$1
-        oldstr=$2
-        newstr=$3
-    else
-        echo "Invalid param number. Usage: greprp [spath] oldstr newstr"
-        exit 1
-    fi
-    grep -r "$oldstr" $spath | awk -F: '{print $1}' | uniq | xargs -i sed -i -E "s|$oldstr|$newstr|g" {}
+  if [ $# -eq 2 ]; then
+    spath='.'
+    oldstr=$1
+    newstr=$2
+  elif [ $# -eq 3 ]; then
+    spath=$1
+    oldstr=$2
+    newstr=$3
+  else
+    echo "Invalid param number. Usage: greprp [spath] oldstr newstr"
+    exit 1
+  fi
+  grep -r "$oldstr" $spath | awk -F: '{print $1}' | uniq | xargs -i sed -i -E "s|$oldstr|$newstr|g" {}
 }
 
 # check command existence
 function checkcmd {
-    command -v $1 1>/dev/null 2>&1
-    return $?
+  command -v $1 1>/dev/null 2>&1
+  return $?
 }
 
 # make dir if not existed
 function mkdir_nowarn {
-    if [ ! -e $1 ]; then mkdir -p $1; fi
+  if [ ! -e $1 ]; then mkdir -p $1; fi
 }
 
 # check and assure sudo access
 function check_sudo_access {
-    prompt=$(sudo -nv 2>&1)
-    if [ $? -eq 0 ]; then
-        info "sudo access has been set"
-    elif echo $prompt | grep -q '^sudo:'; then
-        warn "sudo access required:"
-        sudo -v
-    else
-        error "sudo access not granted" && exit 1
-    fi
+  prompt=$(sudo -nv 2>&1)
+  if [ $? -eq 0 ]; then
+    info "sudo access has been set"
+  elif echo $prompt | grep -q '^sudo:'; then
+    warn "sudo access required:"
+    sudo -v
+  else
+    error "sudo access not granted" && exit 1
+  fi
 }
 
 # get absolute path of current file
 function abspath {
-    echo $(dirname $(realpath $0))
+  echo $(dirname $(realpath $0))
 }
 function absfilepath {
-    echo $(cd ${0%/*} && echo $PWD/${0##*/})
+  echo $(cd ${0%/*} && echo $PWD/${0##*/})
 }
 
 # make trapped temp dir
 function get_temp_dir {
-    TEMP_DIR="$(mktemp -d)"
-    trap 'rm -rf ${TEMP_DIR}' EXIT
-    echo ${TEMP_DIR}
+  TEMP_DIR="$(mktemp -d)"
+  trap 'rm -rf ${TEMP_DIR}' EXIT
+  echo ${TEMP_DIR}
 }
 
 # random uuid
 function random_uuid {
-    NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-    echo ${NEW_UUID}
+  NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  echo ${NEW_UUID}
 }
 
 # random uuid with only lower-case alphanumeric chars.
 function random_uuid_lower {
-    NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-    echo ${NEW_UUID}
+  NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+  echo ${NEW_UUID}
 }
 
 function random_num {
-    WIDTH=${0:-4}
-    NUMBER=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes ${WIDTH})
-    if [ "$NUMBER" == "" ]; then
-        NUMBER=0
-    fi
-    echo ${NUMBER}
+  WIDTH=${0:-4}
+  NUMBER=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes ${WIDTH})
+  if [ "$NUMBER" == "" ]; then
+    NUMBER=0
+  fi
+  echo ${NUMBER}
 }
 
 function current_shell_name {
-    #shellname=$(ps -hp $$ | awk '{print $5}')
-    local ostype=$(uname -s)
-    local username=$(whoami)
-    if [[ $ostype == "Darwin" ]]; then
-        shellname=$(dscl . -read /Users/$username UserShell | awk '{print $2}')
-    else
-        shellname=$(awk -F: -v user="$username" '$1 == user {print $7}' /etc/passwd)
-    fi
-    echo $(basename $shellname)
+  #shellname=$(ps -hp $$ | awk '{print $5}')
+  local ostype=$(uname -s)
+  local username=$(whoami)
+  if [[ $ostype == "Darwin" ]]; then
+    shellname=$(dscl . -read /Users/$username UserShell | awk '{print $2}')
+  else
+    shellname=$(awk -F: -v user="$username" '$1 == user {print $7}' /etc/passwd)
+  fi
+  echo $(basename $shellname)
 }
 
 function current_shell_config {
-    sname=$(current_shell_name)
-    shellconfig="/dev/null"
-    if [[ x$sname == "xzsh" ]]; then
-        shellconfig="$HOME/.zshrc"
-    elif [[ x$sname == "xbash" ]]; then
-        shellconfig="$HOME/.bashrc"
-    fi
-    echo $shellconfig
+  sname=$(current_shell_name)
+  shellconfig="/dev/null"
+  if [[ x$sname == "xzsh" ]]; then
+    shellconfig="$HOME/.zshrc"
+  elif [[ x$sname == "xbash" ]]; then
+    shellconfig="$HOME/.bashrc"
+  fi
+  echo $shellconfig
+}
+
+function latest_release {
+  local repo=$1
+  local proj=$2
+  link=$(curl -s https://api.github.com/repos/$repo/$proj/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+  echo $link
 }
--- tmux/install.sh
+++ tmux/install.sh
@@ -8,54 +8,68 @@ THISDIR=$(dirname $(realpath $0))
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
+TMUX_VERSION=3.4
+
 source $THISDIR/../lib/bash_utils.sh
 
 function usage {
-    echo "Usage: install.sh [-f] [-s] [-e]"
-    echo "  -f copy and install"
-    echo "  -s soft linke install"
-    echo "  -e install dependencies"
-    echo "  -c cleanse install"
+  echo "Usage: install.sh [-f] [-s] [-e]"
+  echo "  -f copy and install"
+  echo "  -s soft linke install"
+  echo "  -e install dependencies"
+  echo "  -c cleanse install"
+}
+
+function install_tmux {
+  info "Installing tmux..."
+  if ! checkcmd tmux; then
+    mkdir_nowarn $HOME/.local/bin
+    mkdir_nowarn /tmp/build-tmux
+    link="https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz"
+    curl -k -L --progress-bar $link | tar xvz -C /tmp/build-tmux/
+    cd /tmp/build-tmux/tmux-${TMUX_VERSION} && ./configure --prefix $HOME/.local && make && cd -
+  fi
 }
 
 function install_tpm {
-    info "Installing tpm..."
-    mkdir_nowarn $XDG_CONFIG_HOME/tmux/plugins
-    if [ ! -e $XDG_CONFIG_HOME/tmux/plugins/tpm ]; then
-        git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
-    fi
+  info "Installing tpm..."
+  mkdir_nowarn $XDG_CONFIG_HOME/tmux/plugins
+  if [ ! -e $XDG_CONFIG_HOME/tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
+  fi
 }
 
 function handle_tmux {
-    mkdir_nowarn $XDG_CONFIG_HOME/tmux
-    if [ x$SOFTLINK == "x1" ]; then
-        ln -sf $THISDIR/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
-        ln -sf $THISDIR/tmux.conf.local $XDG_CONFIG_HOME/tmux/tmux.conf.local
-    else
-        cp $THISDIR/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
-        cp $THISDIR/tmux.conf.local $XDG_CONFIG_HOME/tmux/tmux.conf.local
-    fi
+  mkdir_nowarn $XDG_CONFIG_HOME/tmux
+  if [ x$SOFTLINK == "x1" ]; then
+    ln -sf $THISDIR/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
+    ln -sf $THISDIR/tmux.conf.local $XDG_CONFIG_HOME/tmux/tmux.conf.local
+  else
+    cp $THISDIR/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
+    cp $THISDIR/tmux.conf.local $XDG_CONFIG_HOME/tmux/tmux.conf.local
+  fi
 }
 
 function cleanse_tmux {
-    rm -rf $XDG_CONFIG_HOME/tmux/tmux.conf.local
-    rm -rf $XDG_CONFIG_HOME/tmux/tmux.conf
-    info "All tmux cleansed!"
+  rm -rf $XDG_CONFIG_HOME/tmux/tmux.conf.local
+  rm -rf $XDG_CONFIG_HOME/tmux/tmux.conf
+  info "All tmux cleansed!"
 }
 
 # Change to 0 to install a copy instead of soft link
 SOFTLINK=1
 WITHDEPS=1
 while getopts fsech opt; do
-    case $opt in
-    f) SOFTLINK=0 ;;
-    s) SOFTLINK=1 ;;
-    e) WITHDEPS=1 ;;
-    c) cleanse_tmux && exit 0 ;;
-    h | ?) usage && exit 0 ;;
-    esac
+  case $opt in
+  f) SOFTLINK=0 ;;
+  s) SOFTLINK=1 ;;
+  e) WITHDEPS=1 ;;
+  c) cleanse_tmux && exit 0 ;;
+  h | ?) usage && exit 0 ;;
+  esac
 done
 
+install_tmux
 install_tpm
 handle_tmux
 
