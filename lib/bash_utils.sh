8afda7de5446d25abfb5d3afb35c99c20d7ce2a8
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2024-08-29 11:36:37 +0800

    update logging

--- lib/bash_utils.sh
+++ lib/bash_utils.sh
@@ -4,19 +4,19 @@
 function warn {
   YELLOW='\033[0;33m'
   NC='\033[0m' # No Color
-  printf "${YELLOW}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
+  printf "${YELLOW}[%s] [WARN] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
 }
 
 function info {
   GREEN='\033[0;32m'
   NC='\033[0m' # No Color
-  printf "${GREEN}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
+  printf "${GREEN}[%s] [INFO] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
 }
 
 function error {
   RED='\033[0;31m'
   NC='\033[0m' # No Color
-  printf "${RED}[%s] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
+  printf "${RED}[%s] [ERROR] %s${NC}\n" "$(date '+%Y-%m-%dT%H:%M:%S')" "$@"
 }
 
 function start_install_msg {
