82eaf95906f9b1642133174433ead1cd6f788a42
xiaming.chen <chenxm35@gmail.com>
2024-11-02 23:46:05 +0800

    Use SLIME instead of ELI by default

--- emacs/plugins/lisp-settings.el
+++ emacs/plugins/lisp-settings.el
@@ -5,10 +5,10 @@
 ;;-------
 ;; slime
 ;;-------
-
 (require 'slime)
-;(setq inferior-lisp-program "sbcl")
+
 ;; Allegro CL in modern mode
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "mlisp")
 
 (slime-setup '(slime-fancy slime-quicklisp slime-asdf))
@@ -18,56 +18,11 @@
           (lambda ()
             (define-key slime-prefix-map (kbd "M-h") 'slime-documentation-lookup)))
 
+;;------------
+;; Allegro ELI
+;;------------
 
-;;-----------
-;; Allegro CL
-;;-----------
-(load "/home/chenxm/.local/opt/acl/eli/fi-site-init")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; the following bit of code allows you to prevent from exiting emacs with
-;;; subprocesses running.
-
-(setq kill-emacs-hook
-  (function
-   (lambda ()
-     (save-some-buffers)
-     (my-query-kill-processes))))
-
-(setq my-ignorable-processes
-  '("server" "display-time" "*shell*"))
-
-(defun my-query-kill-processes ()
-  (let ((ps (process-list)))
-    (while ps
-      (if (or (member-equal (process-name (car ps)) my-ignorable-processes)
-	      (not (eq 'run (process-status (car ps))))
-	      (eq 'exit (process-status (car ps)))
-	      (progn
-		(condition-case ()
-		    (progn
-		      (switch-to-buffer (process-buffer (car ps)))
-		      (delete-other-windows)
-		      (end-of-buffer))
-		  (error nil))
-		(y-or-n-p
-		 (format "Kill process %s? " (process-name (car ps))))))
-	  (condition-case ()
-	      (kill-process (car ps))
-	    (error (kill-buffer (process-buffer (car ps))))))
-      (setq ps (cdr ps))))
-  (sleep-for 1))
-
-(defun member-equal (item list)
-  "same as common lisp (member item list :test #'equal)"
-  (let ((ptr list)
-        (done nil)
-        (result '()))
-    (while (not (or done (atom ptr)))
-      (cond ((equal item (car ptr))
-             (setq done t)
-             (setq result ptr)))
-      (setq ptr (cdr ptr)))
-    result))
+; (setq aclisp-home "~/.local/opt/acl")
+; (load (concat aclisp-home "/eli/fi-site-init"))
 
 (provide 'lisp-settings)
